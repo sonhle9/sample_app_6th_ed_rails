4a201b6b2b8558bb9ce1374ea9a6bbd3c08bc876
sonhle9 <sonhle9@gmail.com>
2021-02-21 10:36:14 +0700

    breack out dropdown menu and add cor for 4200 angular

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,6 +16,22 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins 'http://localhost:4200'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins 'http://localhost:8080'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
     origins 'https://railstutorialreact.herokuapp.com'
 
     resource '*',
