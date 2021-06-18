f36cb2e23e5357535015c4ddd6b93b0604ab1571
sonhle9 <sonhle9@gmail.com>
2021-06-19 06:40:17 +0700

    csrf

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,7 +1,15 @@
 class Api::ApiController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
+  before_action :set_csrf_cookie
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery with: :exception
   include SessionsHelper
   private
+    def set_csrf_cookie
+      cookies["CSRF-TOKEN"] = form_authenticity_token
+    end
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
